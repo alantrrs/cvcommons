8b6bcdceebf203fa1af649a315cc44dde538ae3c
ambidextrvs <ambidextrvs@gmail.com>
2013-08-01 15:59:50 -0700

    Add option to set initial frame

--- player_sample.cpp
+++ player_sample.cpp
@@ -2,34 +2,39 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
+
 const char* keys = {
 "{ v  |video     |null| video file }"
+"{ i  |init_frame|0| initial frame }"
 };
 
 int main(int argc, char * argv[]){
-  VideoCapture capture;
+  VideoCapture cam;
  //Read options
   CommandLineParser parser(argc, argv, keys);
   string video = parser.get<string>("v");
-  //Init capture
+  int f0 = parser.get<int>("i");
+  //Init cam
   if (video != "null")
-    capture.open(video);
+    cam.open(video);
   else
-    capture.open(0);
-  if (!capture.isOpened()){
-	  cout << "Capture device failed to open!" << endl;
+    cam.open(0);
+  if (!cam.isOpened()){
+	  cout << "cam device failed to open!" << endl;
     return 1;
   }
-  
+  //Read first frame
   Mat frame;
-  while(capture.read(frame)){
+  cam.set(CV_CAP_PROP_POS_FRAMES,f0);
+  cam.read(frame);
+  while(cam.read(frame)){
   	//Display
     imshow("Player", frame);
     char key = cvWaitKey(33);
     if (key == 'q')
       break;
   }
-  capture.release();
+  cam.release();
   return 0;
 }
 
